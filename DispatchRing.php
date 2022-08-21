0f6c7d4918cd90540e4a4408121354bcdce1a04a
Filippo Montani <78483736+Carnicero90@users.noreply.github.com>
2022-08-21 14:54:29 +0200

    Create DispatchRing.php

---
+++ DispatchRing.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Support\Facades\App;
+use Illuminate\Bus\Queueable;
+// use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+abstract class DispatchRing implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    public $obj;
+    public $rings;
+
+    public function __construct($data)
+    {
+        $this->obj = $data['obj'];
+        $this->rings = $data['rings'];
+    }
+
+    abstract function process();
+
+    public function handle()
+    {
+
+        $this->obj = $this->process($this->obj);
+
+        if (count($this->rings)) {
+            $next = array_shift($this->rings);
+
+            return dispatch(App::makeWith($next, [
+                'data' => [
+                    'obj' => $this->obj,
+                    'rings' => $this->rings,
+                ]
+            ]));
+        }
+
+        return $this->obj;
+    }
+}
